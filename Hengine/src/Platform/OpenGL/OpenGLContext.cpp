c101ae8c376c4431ff6a3ac607047efbf079991d
donghui <452819411@qq.com>
2024-10-16 09:37:27 +0800

    Our first triangle!

--- Hengine/src/Platform/OpenGL/OpenGLContext.cpp
+++ Hengine/src/Platform/OpenGL/OpenGLContext.cpp
@@ -19,6 +19,12 @@ namespace HEngine
 		glfwMakeContextCurrent(m_WindowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		HE_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+		HE_CORE_INFO("OpenGL Info:");
+
+		HE_CORE_INFO("  Vendor: {0}", (const char*)glGetString(GL_VENDOR));
+		HE_CORE_INFO("  Renderer: {0}", (const char*)glGetString(GL_RENDERER));
+		HE_CORE_INFO("  Version: {0}", (const char*)glGetString(GL_VERSION));
 	}
 
 	void OpenGLContext::SwapBuffers()
