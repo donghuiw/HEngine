9c3602b9fdbe87db49b44035118eaa3a39f95d4e
donghui <452819411@qq.com>
2024-10-16 23:20:53 +0800

    Added OpenGL buffer abstraction.

--- Hengine/src/HEngine/Application.h
+++ Hengine/src/HEngine/Application.h
@@ -7,6 +7,7 @@
 #include "Events/ApplicationEvent.h"
 #include "HEngine/ImGui/ImGuiLayer.h"
 #include "HEngine/Renderer/Shader.h"
+#include "HEngine/Renderer/Buffer.h"
 
 namespace HEngine
 {
@@ -31,8 +32,10 @@ namespace HEngine
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 
-		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		unsigned int m_VertexArray;
 		std::unique_ptr<Shader> m_Shader;
+		std::unique_ptr<VertexBuffer> m_VertexBuffer;
+		std::unique_ptr<IndexBuffer> m_IndexBuffer;
 	private:
 		static Application* s_Instance;
 	};
---
+++ Hengine/src/Platform/OpenGL/OpenGLBuffer.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "HEngine/Renderer/Buffer.h"
+
+namespace HEngine
+{
+	class OpenGLVertexBuffer : public VertexBuffer
+	{
+	public:
+		OpenGLVertexBuffer(float* vertices, uint32_t size);
+		virtual ~OpenGLVertexBuffer();
+
+		virtual void Bind() const;
+		virtual void UnBind() const;
+	private:
+		uint32_t m_RendererID;
+	};
+	class OpenGLIndexBuffer : public IndexBuffer
+	{
+	public:
+		OpenGLIndexBuffer(uint32_t* indices, uint32_t size);
+		virtual ~OpenGLIndexBuffer();
+
+		virtual void Bind() const;
+		virtual void UnBind() const;
+
+		virtual uint32_t GetCount() const { return m_Count;  }
+	private:
+		uint32_t m_RendererID;
+		uint32_t m_Count;
+	};
+}
