f53be49468693889bb10a6cb1e4c59f78ba2dcc9
donghui <452819411@qq.com>
2024-11-10 20:35:03 +0800

    Moved OpenGl shader implementation to OpenGLShader class + added more uniform functions

--- HEngine/src/HEngine/Renderer/Renderer.cpp
+++ HEngine/src/HEngine/Renderer/Renderer.cpp
@@ -1,5 +1,6 @@
 #include "hepch.h"
 #include "Renderer.h"
+#include "Platform/OpenGL/OpenGLShader.h"
 
 namespace HEngine
 {
@@ -12,14 +13,13 @@ namespace HEngine
 
 	void Renderer::EndScene()
 	{
-
 	}
 
 	void Renderer::Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray, const glm::mat4& transform)
 	{
 		shader->Bind();
-		shader->UploadUniformMat4("u_ViewProjection", s_SceneData->ViewProjectionMatrix);
-		shader->UploadUniformMat4("u_Transform", transform);
+		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_ViewProjection", s_SceneData->ViewProjectionMatrix);
+		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_Transform", transform);
 
 		vertexArray->Bind();
 		RenderCommand::DrawIndexed(vertexArray);
--- SandBox/src/SandboxApp.cpp
+++ SandBox/src/SandboxApp.cpp
@@ -1,6 +1,11 @@
 #include "HEngine.h"
+
+#include "Platform/OpenGL/OpenGLShader.h"
+
 #include "imgui/imgui.h"
+
 #include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
 
 class ExampleLayer : public HEngine::Layer
 {
@@ -82,9 +87,9 @@ public:
 			}
 		)";
 
-		m_Shader.reset(new HEngine::Shader(vertexSrc, fragmentSrc));
+		m_Shader.reset(HEngine::Shader::Create(vertexSrc, fragmentSrc));
 
-		std::string blueShaderVertexSrc = R"(
+		std::string flatColorShaderVertexSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
@@ -101,22 +106,21 @@ public:
 			}
 		)";
 
-		std::string blueShaderFragmentSrc = R"(
+		std::string flatColorShaderFragmentSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;
 
+			uniform vec3 u_Color;
+
 			void main()
 			{
-				color = vec4(0.2, 0.3, 0.8, 1.0);
+				color = vec4(u_Color, 1.0);
 			}
 		)";
-		 m_BlueShader.reset(new HEngine::Shader(blueShaderVertexSrc, blueShaderFragmentSrc));
-	}
-	virtual void OnImGuiRender() override
-	{
+		 m_FlatColorShader.reset(HEngine::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 	}
 	void OnUpdate(HEngine::Timestep ts) override
 	{
@@ -145,19 +149,28 @@ public:
 
 		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
 
+		std::dynamic_pointer_cast<HEngine::OpenGLShader>(m_FlatColorShader)->Bind();
+		std::dynamic_pointer_cast<HEngine::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat3("u_Color", m_SquareColor);
+
 		for (int y = 0; y < 20; y++)
 		{
 			for (int x = 0; x < 20; x++)
 			{
 				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
 				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
-				HEngine::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+				HEngine::Renderer::Submit(m_FlatColorShader, m_SquareVA, transform);
 			}
 		}
 
 		HEngine::Renderer::Submit(m_Shader, m_VertexArray);
 
 		HEngine::Renderer::EndScene();
+	}
+	virtual void OnImGuiRender() override
+	{
+		ImGui::Begin("Settings");
+		ImGui::ColorEdit3("Square Color", glm::value_ptr(m_SquareColor));
+		ImGui::End();
 	}
 	void OnEvent(HEngine::Event& event) override
 	{
@@ -166,7 +179,7 @@ private:
 	std::shared_ptr<HEngine::Shader> m_Shader;
 	std::shared_ptr<HEngine::VertexArray> m_VertexArray;
 
-	std::shared_ptr<HEngine::Shader> m_BlueShader;
+	std::shared_ptr<HEngine::Shader> m_FlatColorShader;
 	std::shared_ptr<HEngine::VertexArray> m_SquareVA;
 
 	HEngine::OrthographiCamera m_Camera;
@@ -175,6 +188,8 @@ private:
 
 	float m_CameraRotation = 0.0f;
 	float m_CameraRotationSpeed = 180.0f;
+
+	glm::vec3 m_SquareColor = { 0.2f, 0.3f, 0.8f };
 };
 
 
