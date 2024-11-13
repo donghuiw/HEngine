9b9320cb5f3612d3e837a2e5c0fea1ee846c422a
donghui <452819411@qq.com>
2024-11-13 21:11:01 +0800

    Added proper window resize handing

--- HEngine/src/HEngine/Renderer/Renderer.cpp
+++ HEngine/src/HEngine/Renderer/Renderer.cpp
@@ -14,7 +14,10 @@ namespace HEngine
 	{
 		s_SceneData->ViewProjectionMatrix = camera.GetViewProjectionMatrix();
 	}
-
+	void Renderer::OnWindowResize(uint32_t width, uint32_t height)
+	{
+		RenderCommand::SetViewport(0, 0, width, height);
+	}
 	void Renderer::EndScene()
 	{
 	}
--- HEngine/src/HEngine/Renderer/Renderer.h
+++ HEngine/src/HEngine/Renderer/Renderer.h
@@ -12,6 +12,7 @@ namespace HEngine
 		static void Init();
 		static void BeginScene(OrthographiCamera& camera);
 		static void EndScene();
+		static void OnWindowResize(uint32_t width, uint32_t height);
 
 		static void Submit(const Ref<Shader>& shader, const Ref<VertexArray>& vertexArray, const glm::mat4& transform = glm::mat4(1.0f));
 
