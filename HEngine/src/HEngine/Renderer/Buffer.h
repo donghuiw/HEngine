e93833596cc5d970dd83df4e0193ae27ba911599
donghui <452819411@qq.com>
2024-11-27 23:00:28 +0800

    some pull request

--- HEngine/src/HEngine/Renderer/Buffer.h
+++ HEngine/src/HEngine/Renderer/Buffer.h
@@ -66,7 +66,7 @@ namespace HEngine
 	public:
 		BufferLayout() = default;
 
-		BufferLayout(const std::initializer_list<BufferElement>& elements)
+		BufferLayout(std::initializer_list<BufferElement> elements)
 			: m_Elements(elements)
 		{
 			CalculateOffsetsAndStride();
