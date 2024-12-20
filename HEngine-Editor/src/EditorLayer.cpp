99c9d988dd941ddabc18c72be09b4caacf0c67f1
donghui <452819411@qq.com>
2024-12-20 14:24:13 +0800

    Changed KeyPressedEvent repeat count to "isRepeat"

--- HEngine-Editor/src/EditorLayer.cpp
+++ HEngine-Editor/src/EditorLayer.cpp
@@ -420,7 +420,7 @@ namespace HEngine
 	bool EditorLayer::OnKeyPressed(KeyPressedEvent& e)
 	{
 		// Shortcuts
-		if (e.GetRepeatCount() > 0)
+		if (e.IsRepeat())
 			return false;
 
 		bool control = Input::IsKeyPressed(Key::LeftControl) || Input::IsKeyPressed(Key::RightControl);
--- Hengine/src/Platform/Windows/WindowsWindow.cpp
+++ Hengine/src/Platform/Windows/WindowsWindow.cpp
@@ -104,7 +104,7 @@ namespace HEngine
 				}
 				case GLFW_REPEAT:
 				{
-					KeyPressedEvent event(key, 1);
+					KeyPressedEvent event(key, true);
 					data.EventCallback(event);
 					break;
 				}
