745e6aa8996106cf4309c5f4db456f3e6f634c9f
donghuiw <452819411@qq.com>
2025-03-01 15:38:58 +0800

    Exposed TextComponent to C#

--- HEngine/src/HEngine/Scripting/ScriptGlue.cpp
+++ HEngine/src/HEngine/Scripting/ScriptGlue.cpp
@@ -18,15 +18,24 @@
 
 namespace HEngine
 {
+	namespace Utils
+	{
+		std::string MonoStringToString(MonoString* string)
+		{
+			char* cStr = mono_string_to_utf8(string);
+			std::string str(cStr);
+			mono_free(cStr);
+			return str;
+		}
+	}
+	
 	static std::unordered_map<MonoType*, std::function<bool(Entity)>> s_EntityHasComponentFuncs;
 
 #define HE_ADD_INTERNAL_CALL(Name) mono_add_internal_call("HEngine.InternalCalls::" #Name, Name)
 
 		static void NativeLog(MonoString* string, int parameter)
 		{
-			char* cStr = mono_string_to_utf8(string);
-			std::string str(cStr);
-			mono_free(cStr);
+			std::string str = Utils::MonoStringToString(string);
 			std::cout << str << ", " << parameter << std::endl;
 		}
 
@@ -142,6 +151,103 @@ namespace HEngine
 			b2Body_SetType(bodyID, Utils::Rigidbody2DTypeToBox2DBody(bodyType));
 		}
 
+		static MonoString* TextComponent_GetText(UUID entityID)
+		{
+			Scene* scene = ScriptEngine::GetSceneContext();
+			HE_CORE_ASSERT(scene);
+			Entity entity = scene->GetEntityByUUID(entityID);
+			HE_CORE_ASSERT(entity);
+			HE_CORE_ASSERT(entity.HasComponent<TextComponent>());
+
+			auto& tc = entity.GetComponent<TextComponent>();
+			return ScriptEngine::CreateString(tc.TextString.c_str());
+		}
+
+		static void TextComponent_SetText(UUID entityID, MonoString* textString)
+		{
+			Scene* scene = ScriptEngine::GetSceneContext();
+			HE_CORE_ASSERT(scene);
+			Entity entity = scene->GetEntityByUUID(entityID);
+			HE_CORE_ASSERT(entity);
+			HE_CORE_ASSERT(entity.HasComponent<TextComponent>());
+
+			auto& tc = entity.GetComponent<TextComponent>();
+			tc.TextString = Utils::MonoStringToString(textString);
+		}
+
+		static void TextComponent_GetColor(UUID entityID, glm::vec4* color)
+		{
+			Scene* scene = ScriptEngine::GetSceneContext();
+			HE_CORE_ASSERT(scene);
+			Entity entity = scene->GetEntityByUUID(entityID);
+			HE_CORE_ASSERT(entity);
+			HE_CORE_ASSERT(entity.HasComponent<TextComponent>());
+
+			auto& tc = entity.GetComponent<TextComponent>();
+			*color = tc.Color;
+		}
+
+		static void TextComponent_SetColor(UUID entityID, glm::vec4* color)
+		{
+			Scene* scene = ScriptEngine::GetSceneContext();
+			HE_CORE_ASSERT(scene);
+			Entity entity = scene->GetEntityByUUID(entityID);
+			HE_CORE_ASSERT(entity);
+			HE_CORE_ASSERT(entity.HasComponent<TextComponent>());
+
+			auto& tc = entity.GetComponent<TextComponent>();
+			tc.Color = *color;
+		}
+
+		static float TextComponent_GetKerning(UUID entityID)
+		{
+			Scene* scene = ScriptEngine::GetSceneContext();
+			HE_CORE_ASSERT(scene);
+			Entity entity = scene->GetEntityByUUID(entityID);
+			HE_CORE_ASSERT(entity);
+			HE_CORE_ASSERT(entity.HasComponent<TextComponent>());
+
+			auto& tc = entity.GetComponent<TextComponent>();
+			return tc.Kerning;
+		}
+
+		static void TextComponent_SetKerning(UUID entityID, float kerning)
+		{
+			Scene* scene = ScriptEngine::GetSceneContext();
+			HE_CORE_ASSERT(scene);
+			Entity entity = scene->GetEntityByUUID(entityID);
+			HE_CORE_ASSERT(entity);
+			HE_CORE_ASSERT(entity.HasComponent<TextComponent>());
+
+			auto& tc = entity.GetComponent<TextComponent>();
+			tc.Kerning = kerning;
+		}
+
+		static float TextComponent_GetLineSpacing(UUID entityID)
+		{
+			Scene* scene = ScriptEngine::GetSceneContext();
+			HE_CORE_ASSERT(scene);
+			Entity entity = scene->GetEntityByUUID(entityID);
+			HE_CORE_ASSERT(entity);
+			HE_CORE_ASSERT(entity.HasComponent<TextComponent>());
+
+			auto& tc = entity.GetComponent<TextComponent>();
+			return tc.LineSpacing;
+		}
+
+		static void TextComponent_SetLineSpacing(UUID entityID, float lineSpacing)
+		{
+			Scene* scene = ScriptEngine::GetSceneContext();
+			HE_CORE_ASSERT(scene);
+			Entity entity = scene->GetEntityByUUID(entityID);
+			HE_CORE_ASSERT(entity);
+			HE_CORE_ASSERT(entity.HasComponent<TextComponent>());
+
+			auto& tc = entity.GetComponent<TextComponent>();
+			tc.LineSpacing = lineSpacing;
+		}
+
+
 		static bool Input_IsKeyDown(KeyCode keycode)
 		{
 			return Input::IsKeyPressed(keycode);
@@ -208,6 +314,15 @@ namespace HEngine
 			HE_ADD_INTERNAL_CALL(Rigidbody2DComponent_GetLinearVelocity);
 			HE_ADD_INTERNAL_CALL(Rigidbody2DComponent_GetType);
 			HE_ADD_INTERNAL_CALL(Rigidbody2DComponent_SetType);
+
+			HE_ADD_INTERNAL_CALL(TextComponent_GetText);
+			HE_ADD_INTERNAL_CALL(TextComponent_SetText);
+			HE_ADD_INTERNAL_CALL(TextComponent_GetColor);
+			HE_ADD_INTERNAL_CALL(TextComponent_SetColor);
+			HE_ADD_INTERNAL_CALL(TextComponent_GetKerning);
+			HE_ADD_INTERNAL_CALL(TextComponent_SetKerning);
+			HE_ADD_INTERNAL_CALL(TextComponent_GetLineSpacing);
+			HE_ADD_INTERNAL_CALL(TextComponent_SetLineSpacing);
 
 			HE_ADD_INTERNAL_CALL(Input_IsKeyDown);
 		}
